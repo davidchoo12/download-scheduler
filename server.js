f76ec8a1c9a0a0693383fa7908d683be1886d93f
David Choo <davidchoo16@gmail.com>
2018-08-04 22:05:04 +0700

    attempt fix for handling srz fail

--- server.js
+++ server.js
@@ -38,6 +38,12 @@ app.get('/links', async (req, res) => {
         console.log('!match, body:', body);
         const h264Rgx = /Episode \d+ .*http.+?(?=class="external")/g;
         match = body.match(h264Rgx);
+        if (!match) { // if no hevc and no h264
+          return {
+            isHevc: false,
+            eps: []
+          };
+        }
         isHevc = false;
       }
       const textBlock = match[0];
